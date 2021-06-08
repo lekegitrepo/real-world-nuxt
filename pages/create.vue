ae8c46fe806010479eb1f7ed4003a72197e98c12
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-08 20:36:21 +0100

    Create head for the seo

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,19 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create- Real World Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Create events in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
