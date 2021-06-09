f58f4a96c8f643c28d082ae8649d780923941705
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-09 12:17:03 +0100

    Import event-card

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 <script>
+import EventCard from '../components/EventCard.vue'
 export default {
+  components: { EventCard },
   head() {
     return {
       title: 'Event Listing'
