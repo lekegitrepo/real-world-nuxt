2c0f1166c7817d884b03c3e0b223b06f9478ad3a
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-09 12:20:33 +0100

    Add error handling for the api call

--- pages/index.vue
+++ pages/index.vue
@@ -18,12 +18,20 @@ export default {
       title: 'Event Listing'
     }
   },
-  asyncData({ $axios }) {
-    return $axios.get('http://localhost:3000/events').then(response => {
-      return {
-        events: response.data
-      }
-    })
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again!'
+        })
+      })
   }
 }
 </script>
